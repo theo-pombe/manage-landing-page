617b4f8da28034c7bbf48f748c7532d652326433
theo-pombe <theo.pombe@gmail.com>
2023-04-26 22:33:00 +0300

    add testimonial slider.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,3 +7,29 @@ humbergerMenu.addEventListener("click", () => {
   humbergerMenu.classList.toggle("is-open");
   navbarMenu.classList.toggle("hidden");
 });
+
+// TESTIMONIAL SLIDER
+const testimonials = document.querySelectorAll(
+  "#testimonial"
+) as NodeListOf<HTMLDivElement>;
+
+const indicators = document.querySelectorAll(
+  "#indicator"
+) as NodeListOf<HTMLDivElement>;
+
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener("click", (e) => {
+    const currentTestimonial = testimonials[index];
+    const currentIndicator = e.target as HTMLSpanElement;
+
+    testimonials.forEach((testimonial, index) => {
+      testimonial.classList.add("hidden");
+      testimonial.classList.remove("flex");
+      indicators[index].classList.remove("bg-BrightRed");
+    });
+
+    currentTestimonial.classList.add("flex");
+    currentTestimonial.classList.remove("hidden");
+    currentIndicator.classList.add("bg-BrightRed");
+  });
+});
